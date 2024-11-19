0625a51de6f7d10076f1d37abed17ebadec4df10
ParkHyeonDo <qhar1739@naver.com>
2024-11-20 00:10:04 +0900

    Fix , ReFactor : PlayerController

---
+++ Assets/Scripts/Data/CharacterStatHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class CharacterStatHandler : MonoBehaviour
+{
+    [SerializeField] private CharacterStat playerBaseStat;
+    public CharacterStat PlayerCurrentStat;
+
+    private void Start()
+    {
+        PlayerCurrentStat = new CharacterStat();
+        PlayerCurrentStat.Health = playerBaseStat.Health;
+        PlayerCurrentStat.Mana = playerBaseStat.Mana;
+        PlayerCurrentStat.Speed = playerBaseStat.Speed;
+        PlayerCurrentStat.JumpForce = playerBaseStat.JumpForce;
+    }
+}
+
