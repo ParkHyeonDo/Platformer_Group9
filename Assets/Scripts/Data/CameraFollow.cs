0ac394800ca7a1e1a7830c10346cc4e5092fe92e
JeonYE95 <eunzse@gmail.com>
2024-11-20 16:26:25 +0900

    Add : Camera
    카메라 수정중

--- Assets/Scripts/Data/CameraFollow.cs
+++ Assets/Scripts/Data/CameraFollow.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform PlayerTrans;
     public Vector3 Offset;
+    private Rigidbody2D cameraRb;
 
     private void Start()
     {
@@ -16,13 +18,16 @@ public class CameraFollow : MonoBehaviour
         {
             Offset = transform.position - PlayerTrans.position;
         }
+
+        cameraRb = GetComponentInChildren<Rigidbody2D>();
     }
 
     private void LateUpdate()
     {
-        if (PlayerTrans != null)
+        if (PlayerTrans != null && cameraRb != null)
         {
-            transform.position = PlayerTrans.position + Offset;
+            Vector3 targetPosition = PlayerTrans.position + Offset;
+            cameraRb.MovePosition(new Vector2(targetPosition.x, targetPosition.y));
         }
     }
 }
